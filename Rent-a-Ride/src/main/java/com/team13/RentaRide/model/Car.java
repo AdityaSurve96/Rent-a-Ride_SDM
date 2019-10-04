0ef01817c44f0aaba40d9006834d8a819e0c3abf
pranotimulay <55164245+pranotimulay@users.noreply.github.com>
2019-10-04 13:10:55 -0400

    MODEL-CLASSES+POM-DEPENDENCIES
    Added model package with a Car model inside, added Spring Test and Junit dependencies in the pom.

---
+++ Rent-a-Ride/src/main/java/com/team13/RentaRide/model/Car.java
@@ -0,0 +1,77 @@
+package com.team13.RentaRide.model;
+
+import java.math.BigDecimal;
+
+public class Car {
+
+	private String name;
+	private String model;
+	private String licenseNumber;
+	private String color;
+	private String description;
+	private BigDecimal price;
+	boolean isAvailable;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getLicenseNumber() {
+		return licenseNumber;
+	}
+
+	public void setLicenseNumber(String licenseNumber) {
+		this.licenseNumber = licenseNumber;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [name=" + name + ", model=" + model + ", licenseNumber=" + licenseNumber + ", color=" + color
+				+ ", description=" + description + ", price=" + price + ", isAvailable=" + isAvailable + "]";
+	}
+
+}
