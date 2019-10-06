a570b2e0f6d51288aa25b63e9f949e5125434f04
AdityaSurve96 <aditya.surve2913@gmail.com>
2019-10-06 12:10:22 -0400

    Created a controller for RentCar jsp Page

--- Rent-a-Ride/src/main/java/com/team13/RentaRide/Controller/RentCarController.java
+++ Rent-a-Ride/src/main/java/com/team13/RentaRide/Controller/RentCarController.java
@@ -44,6 +44,9 @@ public class RentCarController {
 
 	}
 	
+	
+	
+	
 	@RequestMapping(value= "/rentTheSelectedCarfalse")
 	public ModelAndView showCatalogPage(@RequestParam String licensePlate) {
 		DataStore ds = DataStore.getInstance();
@@ -58,29 +61,37 @@ public class RentCarController {
 
 	}
 
+	
+	
+	
+	
+	
 	@RequestMapping(value ="/RentCarForClient",  method = RequestMethod.POST)
 	public ModelAndView showRentedCars(@RequestParam String clientFirstName,@RequestParam String clientLastName,
 			@RequestParam String phoneNumber,@RequestParam String driverLicenceNumber,
 			@RequestParam String licenceExpiryDate, @RequestParam String CarLicenseNoForForm,
 			@RequestParam String dueDate) {
+		
+		Integer phnumber = Integer.parseInt(phoneNumber);
 
 		String licnum = CarLicenseNoForForm;
 		DataStore ds = DataStore.getInstance();
 		List<Car> carsList = ds.getAllCars();
 		Car c=null;
+		
 		for (Car car : carsList) {
 			String lic = car.getLicensePlateNumber();
 
 			if(lic.equals(licnum)) {
-
+				car.setAvailable(false);
+				car.setAvailableToRentOrNot(car.isAvailable());
 				c = car;
 				break;
-
+				
 			}
 		}
 
-		Integer phnumber = Integer.parseInt(phoneNumber);
-
+	
 		Client cl = new Client(driverLicenceNumber, clientFirstName, clientLastName, phnumber, licenceExpiryDate );
 		ModelAndView modelAndView = new ModelAndView("RentedCarList");
 		RentedCarHolder rh = RentedCarHolder.getInstance();
@@ -94,6 +105,8 @@ public class RentCarController {
 
 
 
+	
+	
 	@RequestMapping(value = "/backToCarCatalog")
 	public ModelAndView goToCarCatalog() {
 
@@ -106,17 +119,17 @@ public class RentCarController {
 		List<Car> carsList = ds.getAllCars();
 	
 		
-		for (RentedCar rencar : renCars) {
-			Car rC = rencar.getRentedCar();
-			
-			for (Car car : carsList) {
-				if(rC.getLicensePlateNumber().equals(car.getLicensePlateNumber())) {
-					
-					car.setAvailable(false);
-					car.setAvailableToRentOrNot(car.isAvailable());
-				}
-			}
-		}
+//		for (RentedCar rencar : renCars) {
+//			Car rC = rencar.getRentedCar();
+//			
+//			for (Car car : carsList) {
+//				if(rC.getLicensePlateNumber().equals(car.getLicensePlateNumber())) {
+//					
+//					car.setAvailable(false);
+//					car.setAvailableToRentOrNot(car.isAvailable());
+//				}
+//			}
+//		}
 
 		modelAndView.addObject("cars", carsList);
 	
@@ -124,6 +137,10 @@ public class RentCarController {
 
 
 	}
+	
+	
+	
+	
 	
 	
 	@RequestMapping(value= "/backToRentedCarList")
@@ -137,7 +154,80 @@ public class RentCarController {
 		return modelAndView;
  		
 	}
+	
+
+	
+	
+	
+	
+	
+	
+	@RequestMapping(value = "/modifyCarDetailView1" , method = RequestMethod.POST)
+	public ModelAndView Modify1(@RequestParam String modifyCarNumber1){
+		
+		Integer index  = Integer.parseInt(modifyCarNumber1);
+		ModelAndView modelAndView = setFieldforModification(index);
+			
+		return modelAndView;
+	}
+	@RequestMapping(value = "/modifyCarDetailView" , method = RequestMethod.POST)
+	public ModelAndView Modify2(@RequestParam String modifyCarNumber2){
+		
+		Integer index  = Integer.parseInt(modifyCarNumber2);
+		ModelAndView modelAndView = setFieldforModification(index);
+			
+		return modelAndView;
+	}
+	@RequestMapping(value = "/modifyCarDetailView3" , method = RequestMethod.POST)
+	public ModelAndView Modify3(@RequestParam String modifyCarNumber3){
+		
+		Integer index  = Integer.parseInt(modifyCarNumber3);
+		ModelAndView modelAndView = setFieldforModification(index);
+			
+		return modelAndView;
+	}
+	@RequestMapping(value = "/modifyCarDetailView4" , method = RequestMethod.POST)
+	public ModelAndView Modify4(@RequestParam String modifyCarNumber4){
+		
+		Integer index  = Integer.parseInt(modifyCarNumber4);
+		ModelAndView modelAndView = setFieldforModification(index);
+			
+		return modelAndView;
+	}
+	@RequestMapping(value = "/modifyCarDetailView5" , method = RequestMethod.POST)
+	public ModelAndView Modify5(@RequestParam String modifyCarNumber5){
+		
+		Integer index  = Integer.parseInt(modifyCarNumber5);
+		ModelAndView modelAndView = setFieldforModification(index);
+			
+		return modelAndView;
+	}
+	
+	
+	
+	
+	
+	public ModelAndView setFieldforModification(Integer index) {
+		
+		ModelAndView modelAndView = new ModelAndView("Modify");
+		RentedCarHolder rh = RentedCarHolder.getInstance();
+		List<RentedCar> rentedCarList = rh.getRentals();
+		RentedCar rCar = rentedCarList.get(index-1);
+		Car theCar = rCar.getRentedCar();
+		Client theClient = rCar.getCarsClient();
+		modelAndView.addObject("carLicenseNumber",theCar.getLicensePlateNumber());
+		modelAndView.addObject("clientFirstName",theClient.getClientFirstName());
+		modelAndView.addObject("clientLastName",theClient.getClientLastName());
+		modelAndView.addObject("phoneNumber",theClient.getPhoneNumber());
+		modelAndView.addObject("driverLicenceNumber",theClient.getDriverLicenceNumber());
+		modelAndView.addObject("licenceExpiryDate",theClient.getLicenceExpiryDate());
+		modelAndView.addObject("dueDate",rCar.getDueDate());
+	
+		return modelAndView;
+		
+	}
 }
+
 
 
 
