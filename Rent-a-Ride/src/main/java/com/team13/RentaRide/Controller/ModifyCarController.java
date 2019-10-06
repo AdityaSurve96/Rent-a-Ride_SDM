71178e9aa715ac4fbcd36554bc4c7980111f077b
AshutoshGunjal <divyapandit13@gmail.com>
2019-10-06 11:56:59 -0400

    Added a class for Modify CarRental Controller

--- Rent-a-Ride/src/main/java/com/team13/RentaRide/Controller/ModifyCarController.java
+++ Rent-a-Ride/src/main/java/com/team13/RentaRide/Controller/ModifyCarController.java
@@ -1,39 +1,92 @@
 package com.team13.RentaRide.Controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Iterator;
+import java.util.List;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.team13.RentaRide.model.ClerksHolder;
+import com.team13.RentaRide.model.Car;
+import com.team13.RentaRide.model.Client;
+import com.team13.RentaRide.model.RentedCar;
+import com.team13.RentaRide.model.RentedCarHolder;
 import com.team13.RentaRide.utils.DataStore;
 
 @Controller
-
-
 public class ModifyCarController {
 	
-	@RequestMapping(value = "/Modify" , method = RequestMethod.POST)
-	public ModelAndView Modify1(@RequestParam String driverLicenceNumber, @RequestParam String licensePlateNumber,@RequestParam String firstName, @RequestParam String lastName,@RequestParam String phoneNumber){
-
-		return new ModelAndView("car-catalog-info-page");
+	
+	
+	
+	@RequestMapping(value = "/gotoRentalsPageAfterModify" , method = RequestMethod.POST)
+	public ModelAndView ModifyRental(@RequestParam String CarLicenseNoForForm, @RequestParam String clientFirstName, 
+									 @RequestParam String clientLastName, @RequestParam String phoneNumber,
+									 @RequestParam String driverLicenceNumber, @RequestParam String licenceExpiryDate,
+									 @RequestParam String dueDate){
+		
+		ModelAndView modelAndView = new ModelAndView("RentedCarList");
+		Car theCar=null;
+		Client theClient=null;
+		RentedCarHolder rh = RentedCarHolder.getInstance();
+		List<RentedCar> carList = rh.getRentals();
+		RentedCar rc=null;
+		for (RentedCar rentedCar : carList) {
+			if(rentedCar.getRentedCar().getLicensePlateNumber().equals(CarLicenseNoForForm)) {
+				theCar = rentedCar.getRentedCar();
+				theClient = rentedCar.getCarsClient();
+				rc = rentedCar;
+				break;
+			}
+		}
+		
+		theClient.setClientFirstName(clientFirstName);
+		theClient.setClientLastName(clientLastName);
+		theClient.setDriverLicenceNumber(driverLicenceNumber);
+		theClient.setLicenceExpiryDate(licenceExpiryDate);
+		theClient.setPhoneNumber(Integer.parseInt(phoneNumber));
+		rc.setDueDate(dueDate);
+ 
+		
+		modelAndView.addObject("rentals", rh.getRentals());
+		
+		return modelAndView;
 	}
 	
 	
-	
-	@RequestMapping(value = "/Delete" , method = RequestMethod.POST)
-	public ModelAndView Modify2(@RequestParam String driverLicenceNumber, @RequestParam String licensePlateNumber,@RequestParam String firstName, @RequestParam String lastName,@RequestParam String phoneNumber){
-
-		return new ModelAndView("car-catalog-info-page");
+	@RequestMapping(value = "/gotoRentalsPageAfterDelete" , method = RequestMethod.POST)
+	public ModelAndView DeleteRentals(@RequestParam String CarLicenseNoForForm, @RequestParam String clientFirstName, 
+									  @RequestParam String clientLastName, @RequestParam String phoneNumber,
+									  @RequestParam String driverLicenceNumber, @RequestParam String licenceExpiryDate,
+									  @RequestParam String dueDate){
+		
+		RentedCarHolder rh = RentedCarHolder.getInstance();
+		List<RentedCar> rentedList = rh.getRentals();
+		Car theCar=null;
+		Client theClient = null;
+		RentedCar deletedRental = null;
+		
+		for (RentedCar rentedItem : rentedList) {
+			
+			if(rentedItem.getRentedCar().getLicensePlateNumber().equals(CarLicenseNoForForm)) {
+				rentedList.remove(rentedItem);
+				deletedRental = rentedItem;
+				break;
+			}
+		}
+		DataStore d = DataStore.getInstance();
+		List<Car> cars =  d.getAllCars();
+		
+		cars.add(deletedRental.getRentedCar());
+		
+		ModelAndView modelAndView = new ModelAndView("RentedCarList");
+		
+		modelAndView.addObject("rentals",rh.getRentals());
+		
+		return modelAndView;
 	}
-	
-	
 	
 	
 	
