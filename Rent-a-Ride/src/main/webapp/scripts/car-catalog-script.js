ad888583efc9a914fa6670e43c0128d6f14f8040
kundana2201 <kundanagangam@gmail.com>
2019-10-26 21:15:41 -0400

    Separated viewRentals and viewReservations and addded a javascript function to restrict dates

--- Rent-a-Ride/src/main/webapp/scripts/car-catalog-script.js
+++ Rent-a-Ride/src/main/webapp/scripts/car-catalog-script.js
@@ -137,3 +137,22 @@ function filterCars() {
 	}
 
 }
+
+
+function getCurrentDate() {
+	
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; 
+	var yyyy = today.getFullYear();
+	 if(dd<10){
+	        dd='0'+dd
+	    } 
+	    if(mm<10){
+	        mm='0'+mm
+	    } 
+
+	today = yyyy+'-'+mm+'-'+dd;
+	console.log(today);
+	document.getElementByID("now").setAttribute("min", today);
+}
