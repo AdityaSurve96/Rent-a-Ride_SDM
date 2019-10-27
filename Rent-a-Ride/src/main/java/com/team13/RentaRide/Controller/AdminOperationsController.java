adee2220c9b0e25d602219b18b16ca234ef23820
AshutoshGunjal <ashutosh.r.gunjal@gmail.com>
2019-10-27 16:27:10 -0400

    fiters on car rental page for admin, commit issue

--- Rent-a-Ride/src/main/java/com/team13/RentaRide/Controller/AdminOperationsController.java
+++ Rent-a-Ride/src/main/java/com/team13/RentaRide/Controller/AdminOperationsController.java
@@ -1,10 +1,13 @@
 package com.team13.RentaRide.Controller;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,20 +35,21 @@ public class AdminOperationsController {
 		return modelAndView;
 	}
 
-	
 	@RequestMapping("/adminViewRentals")
 	public ModelAndView showRentals() {
 
-		ModelAndView modelAndView = new ModelAndView("AdminViewRentalTransactions","rentals",DataStore.getInstance().getRentedCars());
-		
+		ModelAndView modelAndView = new ModelAndView("AdminViewRentalTransactions", "rentals",
+				DataStore.getInstance().getRentedCars());
+
 		return modelAndView;
 	}
-	
+
 	@RequestMapping("/adminViewReservations")
 	public ModelAndView showReservations() {
 
-		ModelAndView modelAndView = new ModelAndView("AdminViewReservedTransactions","reservations",DataStore.getInstance().getReservedCars());
-		
+		ModelAndView modelAndView = new ModelAndView("AdminViewReservedTransactions", "reservations",
+				DataStore.getInstance().getReservedCars());
+
 		return modelAndView;
 	}
 
@@ -91,7 +95,7 @@ public class AdminOperationsController {
 		return modelAndView;
 
 	}
-		
+
 	@RequestMapping(value = "/adminManageCatalog", method = RequestMethod.GET)
 	public ModelAndView adminManageCatalog() {
 		return new ModelAndView("AdminCarCatalogPage", "cars", DataStore.getInstance().getAllCars());
@@ -141,7 +145,7 @@ public class AdminOperationsController {
 	public ModelAndView editCar(@RequestParam String currentLicensePlateNumber) {
 		System.out.println("licensePlateNumber: " + currentLicensePlateNumber);
 		Car currentCar = null;
-		boolean flag= false;
+		boolean flag = false;
 		ModelAndView modelAndView = null;
 		for (Car car : DataStore.getInstance().getAllCars()) {
 			if (car.getLicensePlateNumber().equals(currentLicensePlateNumber)) {
@@ -152,31 +156,30 @@ public class AdminOperationsController {
 		List<RentedCar> renCars = DataStore.getInstance().getRentedCars();
 		List<ReservedCar> resCars = DataStore.getInstance().getReservedCars();
 		for (ReservedCar reservedCar : resCars) {
-			if(reservedCar.getCar().equals(currentCar)) {
-				flag=true;
+			if (reservedCar.getCar().equals(currentCar)) {
+				flag = true;
 				break;
 			}
 		}
 		for (RentedCar rentedCar : renCars) {
-			if(rentedCar.getCar().equals(currentCar)) {
-				flag = true; 
+			if (rentedCar.getCar().equals(currentCar)) {
+				flag = true;
 				break;
 			}
 		}
-		
+
 		modelAndView = new ModelAndView("EditCarPage", "car", currentCar);
-		if(flag) {
-			
+		if (flag) {
+
 			modelAndView.addObject("editDecide", "Cannot Edit Car Details, Car cuurently Booked");
-			
+
 			modelAndView.addObject("readOnly", "readonly");
-			modelAndView.addObject("disableOrNo" , "disabled");
-			
-		}
-		else {
+			modelAndView.addObject("disableOrNo", "disabled");
+
+		} else {
 			modelAndView.addObject("editDecide", "Edit Car Details");
 		}
-		 return modelAndView;
+		return modelAndView;
 	}
 
 	@RequestMapping(value = "/saveCarChanges", method = RequestMethod.POST)
@@ -212,7 +215,7 @@ public class AdminOperationsController {
 //		}
 		boolean flag = false;
 		ModelAndView modelAndView = null;
-		Car currentCar=null;
+		Car currentCar = null;
 		for (Car car : DataStore.getInstance().getAllCars()) {
 			if (car.getLicensePlateNumber().equals(currentLicensePlateNumber)) {
 				currentCar = car;
@@ -221,66 +224,108 @@ public class AdminOperationsController {
 		}
 		List<RentedCar> renCars = DataStore.getInstance().getRentedCars();
 		List<ReservedCar> resCars = DataStore.getInstance().getReservedCars();
-		
+
 		for (ReservedCar reservedCar : resCars) {
-			if(reservedCar.getCar().equals(currentCar)) {
-				flag=true;
+			if (reservedCar.getCar().equals(currentCar)) {
+				flag = true;
 				break;
 			}
 		}
 		for (RentedCar rentedCar : renCars) {
-			if(rentedCar.getCar().equals(currentCar)) {
-				flag = true; 
+			if (rentedCar.getCar().equals(currentCar)) {
+				flag = true;
 				break;
 			}
 		}
 		List<Car> allCars = DataStore.getInstance().getAllCars();
-		if(flag) {
-			
-		 modelAndView =  new ModelAndView("AdminCarCatalogPage", "cars", allCars);
-		 modelAndView.addObject("errorMessage", "Cannot delete vehicle now  as it is currently booked");
-		 return modelAndView;
-		}
-		else {
-			
+		if (flag) {
+
+			modelAndView = new ModelAndView("AdminCarCatalogPage", "cars", allCars);
+			modelAndView.addObject("errorMessage", "Cannot delete vehicle now  as it is currently booked");
+			return modelAndView;
+		} else {
+
 			allCars.remove(currentCar);
 			return new ModelAndView("AdminCarCatalogPage", "cars", allCars);
 		}
 	}
-	
 
-	
-	@RequestMapping(value= "/backToAdminReservedCarList")
+	@RequestMapping(value = "/backToAdminReservedCarList")
 	public ModelAndView showAdminReservedCarsPage() {
-		
+
 		ModelAndView modelAndView = new ModelAndView("AdminViewReservedTransactions");
 		DataStore ds = DataStore.getInstance();
 		modelAndView.addObject("reservations", ds.getReservedCars());
 		return modelAndView;
- 		
+
 	}
-	
-	@RequestMapping(value= "/backToAdminRentedCarList")
+
+	@RequestMapping(value = "/backToAdminRentedCarList")
 	public ModelAndView showAdminRentedCarsPage() {
-		
+
 		ModelAndView modelAndView = new ModelAndView("AdminViewRentalTransactions");
-		DataStore ds= DataStore.getInstance();
+		DataStore ds = DataStore.getInstance();
 		modelAndView.addObject("rentals", ds.getRentedCars());
 		return modelAndView;
- 		
+
 	}
-	
+
 	@RequestMapping(value = "/backToAdminCarCatalog")
 	public ModelAndView goToAdminCarCatalog() {
 
-		ModelAndView modelAndView  = new ModelAndView("AdminCarCatalogPage","cars",DataStore.getInstance().getAllCars());
-		
+		ModelAndView modelAndView = new ModelAndView("AdminCarCatalogPage", "cars",
+				DataStore.getInstance().getAllCars());
+
 		DataStore ds = DataStore.getInstance();
 		List<Car> carsList = ds.getAllCars();
-		
+
 		modelAndView.addObject("cars", carsList);
-		
+
 		return modelAndView;
-		
+
+	}
+
+	@RequestMapping(value = "/filterRentalRecordsForAdmin", method = RequestMethod.POST)
+	public ModelAndView filterRentalRecordsForAdmin(@RequestParam String licensePlateNumberInput,
+			@RequestParam String drivingLicenseNumberInput,
+			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dueDateFilter,
+			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate pickupDateFilter
+//			
+	) {
+
+		ModelAndView modelAndView = new ModelAndView("AdminViewRentalTransactions");
+		List<RentedCar> rentals = DataStore.getInstance().getRentedCars();
+
+		List<RentedCar> rentalsToSend = new ArrayList<>();
+
+		for (RentedCar rental : rentals) {
+			if (!StringUtils.isEmpty(licensePlateNumberInput)
+					&& !licensePlateNumberInput.equals(rental.getCar().getLicensePlateNumber())) {
+//				System.out.println("setting model add false");
+				continue;
+			}
+
+			if (!StringUtils.isEmpty(drivingLicenseNumberInput)
+					&& !drivingLicenseNumberInput.equals(rental.getAssociatedClient().getDriverLicenceNumber())) {
+//				System.out.println("setting model add false");
+				continue;
+			}
+
+			if (dueDateFilter != null && !dueDateFilter.equals(rental.getDueDate())) {
+//				System.out.println("setting model add false");
+				continue;
+			}
+
+			if (pickupDateFilter != null && !pickupDateFilter.equals(rental.getStartDate())) {
+//				System.out.println("setting model add false");
+				continue;
+			}
+			rentalsToSend.add(rental);
+
+		}
+
+		modelAndView.addObject("rentals", rentalsToSend);
+		return modelAndView;
+
 	}
 }
